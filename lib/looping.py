f13a2bc7ef7d092a395b4d8483548252603f4740
Sandra Misigo <misigosandra@gmail.com>
2025-02-13 00:42:36 +0300

    python-p3-loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [x**2 for x in int_list]
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):  # Loop through numbers from 1 to 100
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")  # Multiple of both 3 and 5
+        elif i % 3 == 0:
+            print("Fizz")  # Multiple of 3
+        elif i % 5 == 0:
+            print("Buzz")  # Multiple of 5
+        else:
+            print(i)  # Not a multiple of 3 or 5, print the number
